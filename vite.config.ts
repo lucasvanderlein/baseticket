70e31abb13e1c86fe82738d1d39507421e6de2a2
lucasvanderlein <lucasvanderlein@users.noreply.github.com>
2025-10-24 11:51:55 +0300

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/base-ticket-forge/' : '/',
+  base: mode === 'production' ? '/baseticket/' : '/',
   server: {
     host: "::",
     port: 8080,
